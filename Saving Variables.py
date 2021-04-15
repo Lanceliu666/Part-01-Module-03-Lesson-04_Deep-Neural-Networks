0592c40b1df70e7979177f116cc2b4e49248b062
Lanceliu666 <77369108+Lanceliu666@users.noreply.github.com>
2021-04-15 16:42:18 +0800

    Create Saving Variables.py

---
+++ Saving Variables.py
@@ -0,0 +1,55 @@
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.compat.v1.disable_eager_execution()
+
+# The file path to save the data
+save_file = './model.ckpt'
+
+# Two Tensor Variables: weights and bias
+weights = tf.Variable(tf.truncated_normal([2, 3]))
+bias = tf.Variable(tf.truncated_normal([3]))
+
+# Class used to save and/or restore Tensor Variables
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    # Initialize all the Variables
+    sess.run(tf.global_variables_initializer())
+
+    # Show the values of weights and bias
+    print('Weights:')
+    print(sess.run(weights))
+    print('Bias:')
+    print(sess.run(bias))
+
+    # Save the model
+    saver.save(sess, save_file)
+
+
+#Weights:
+
+#[[-0.97990924 1.03016174 0.74119264][-0.82581609 -0.07361362 -0.86653847]]
+
+#Bias:
+
+#[ 1.62978125 -0.37812829 0.64723819]
+
+# Remove the previous weights and bias
+tf.reset_default_graph()
+
+# Two Variables: weights and bias
+weights = tf.Variable(tf.truncated_normal([2, 3]))
+bias = tf.Variable(tf.truncated_normal([3]))
+
+# Class used to save and/or restore Tensor Variables
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    # Load the weights and bias
+    saver.restore(sess, save_file)
+
+    # Show the values of weights and bias
+    print('Weight:')
+    print(sess.run(weights))
+    print('Bias:')
+    print(sess.run(bias))
